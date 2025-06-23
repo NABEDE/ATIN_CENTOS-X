2b5b0f070a6dee174577fb8a7116a03bfd1c5bd2
NABEDE lidawo Jérôme Bienvenu <nabede16@gmail.com>
2025-06-23 19:01:18 +0100

    Variables.sh

--- test/test.variables.sh
+++ test/test.variables.sh
@@ -6,8 +6,14 @@ echo -e "${RED} Hello World ${NC}"
 
 echo $YELLOW
 
+echo -e "${YELLOW} Hello World ${NC}"
+
 echo $BLUE
 
+echo -e "${BLUE} Hello World ${NC}"
+
 echo $GREEN
+
+echo -e "${GREEN} Hello world ${NC}"
 
 echo $NC
